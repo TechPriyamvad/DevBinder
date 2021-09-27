cf294881d43813ce2bfba93709cf7ba02071efc0
Technical Priyam <priyamvadofficial1@gmail.com>
2021-09-27 22:41:52 +0530

    added middleware for user authetication and jwt verification

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,7 +1,22 @@
 // handling request coming from auth route
 const express = require("express");
 const authRouter = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 
-authRouter.get("/", (req, res) => res.status(200).send("Auth route"));
+// @route    GET api/auth
+// @desc     Get user by token
+// @access   Private
+authRouter.get("/", auth, async (req, res) => {
+  try {
+    // check  if any user exist with the id given by token
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(400).send("user doesn't exist");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = authRouter;
