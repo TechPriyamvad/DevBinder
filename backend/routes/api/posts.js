ee6389ee0dc132af0261b4ec21be6fd45c954197
Technical Priyam <priyamvadofficial1@gmail.com>
2021-09-30 20:27:36 +0530

    like and unlike a post

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -108,5 +108,113 @@ postRouter.delete("/:id", auth, async (req, res) => {
   }
 });
 
+// @route    PUT api/posts/like/:id
+// @desc     Like a post
+// @access   Private
+postRouter.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //   check if post exist in collection
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
 
+    // Check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    return res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind==='ObjectId') {
+        return res.status(404).json({ msg: "Post not found" });
+      }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    PUT api/posts/unlike/:id
+// @desc     Unlike a post
+// @access   Private
+postRouter.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+        return res.status(404).json({ msg: "Post not found" });
+      }
+    
+    // Check if the post has not yet been liked
+    if (post.likes.filter((like) => like.user.toString() === req.user.id).length==0) {
+      return res.status(400).json({ msg: "Post has not yet been liked" });
+    }
+
+    // remove the like
+    post.likes = post.likes.filter(
+      ({ user }) => user.toString() !== req.user.id
+    );
+
+    await post.save();
+
+    return res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind==='ObjectId') {
+        return res.status(404).json({ msg: "Post not found" });
+      }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    POST api/posts/comment/:id
+// @desc     Comment on a post
+// @access   Private
+postRouter.post(
+    '/comment/:id',
+    auth,
+    check('text', 'Text is required').notEmpty(),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      try {
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id);
+
+        // check if post exist
+        if (!post) {
+            return res.status(404).json({ msg: "Post not found" });
+          }
+
+        const newComment = {
+          text: req.body.text,
+          name: user.name,
+          avatar: user.avatar,
+          user: req.user.id
+        };
+  
+        post.comments.unshift(newComment);
+  
+        await post.save();
+  
+        res.json(post.comments);
+      } catch (err) {
+        console.error(err.message);
+        if (err.kind==='ObjectId') {
+            return res.status(404).json({ msg: "Post not found" });
+          }
+        res.status(500).send('Server Error');
+      }
+    }
+  );
 module.exports = postRouter;
